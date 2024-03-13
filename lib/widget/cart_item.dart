e396d76f7742e059fdd685d9d0c25d2795136702
akhsak <akhsamariyam2k2@gmail.com>
2024-03-13 13:00:12 +0530

    add

--- lib/widget/cart_item.dart
+++ lib/widget/cart_item.dart
@@ -23,7 +23,7 @@ class CartItem extends StatelessWidget {
                 height: mediaQuery.height * .1,
                 margin: const EdgeInsets.only(right: 5),
                 child: Image.asset(
-                  '',
+                  'assets/baby_welcome.png.jpeg',
                   fit: BoxFit.cover,
                 ),
               ),
